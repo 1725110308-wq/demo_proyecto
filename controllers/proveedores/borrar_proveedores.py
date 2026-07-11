f6fe70395c89a817581aaa12bad9f4b6f9cbcccb
Saúl <1725110308@utectulancingo.edu.mx>
2026-07-11 21:47:11 +0000

    UPDATE se completo la funcionalidad de borrar

--- controllers/proveedores/borrar_proveedores.py
+++ controllers/proveedores/borrar_proveedores.py
@@ -3,13 +3,32 @@ import sqlite3
 render = web.template.render('views/proveedores', base='layout')
 
 class BorrarProveedores:
-    def mostrarPoveedor(self,id_proovedor):
+    def borrarProveedor(self,proveedor)->bool:
+        try:
+            conexion=sqlite3.connect("sql/ferretariasaul.db")
+            conexion.row_factory=sqlite3.Row
+            cursor=conexion.cursor()
+            id_proveedor=proveedor
+            query="""DELETE FROM proveedores
+                     where id_proveedor=?"""
+            cursor.execute(query,(id_proveedor),)
+            conexion.commit()
+            conexion.close
+            return True
+        except sqlite3.Error as error:
+            print(f"ERROR 5: {error.args}")
+            return False
+        except Exception as errror:
+            print(f"ERROR 6: {errror.args}")
+            return False
+
+    def mostrarPoveedor(self,id_provedor):
         try:
             conexion = sqlite3.connect("sql/ferretariasaul.db")
             conexion.row_factory = sqlite3.Row
             cursor = conexion.cursor()
             query = "SELECT * FROM proveedores where id_proveedor = ?;"
-            cursor.execute(query,(id_proovedor,))
+            cursor.execute(query,(id_provedor,))
             resultado = cursor.fetchone()
 
             proveedor = {
@@ -29,7 +48,16 @@ class BorrarProveedores:
         except Exception as error:
             print(f"ERROR 4: {error.args}")
             return {}
-    def GET(self, id_proveedor):
+        
+    def GET(self, id_proveedor:int):
         print(f"id del proveedor: {id_proveedor}")
         proveedor = self.mostrarPoveedor(id_proveedor)
         return render.borrar_proveedores(proveedor)
+    
+    def POST(self,id_proveedor:int):
+        formulario=web.input()
+        proveedor=formulario['id_proveedor']
+        resultado=self.borrarProveedor(proveedor)
+        web.ctx.status = '303 See Other'
+        web.header('Location', '/ver_proveedores')
+        return''
