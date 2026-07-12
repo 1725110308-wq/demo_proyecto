0100a94e9fa75f6d999bf6ae297b45dcb49efa2d
Saúl <1725110308@utectulancingo.edu.mx>
2026-07-12 21:23:36 +0000

    CREATED se añadio css

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import web
 
 urls = (
+    '/css/(.*)', 'Static',
     '/', 'controllers.index.Index',
     '/ver_proveedores', 'controllers.proveedores.ver_proveedores.VerProveedores',
     '/borrar_proveedores/(.*)', 'controllers.proveedores.borrar_proveedores.BorrarProveedores',
@@ -9,6 +10,15 @@ urls = (
 )
 
 app = web.application(urls, globals())
+
+class Static:
+    def GET(self, filename):
+        try:
+            with open(f'css/{filename}', 'rb') as f:
+                web.header('Content-Type', 'text/css')
+                return f.read()
+        except IOError:
+            raise web.notfound()
 
 if __name__ == '__main__':
     app.run()
