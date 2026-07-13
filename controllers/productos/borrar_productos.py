8180b797cea1f977c442c0bc543902e5af22d320
Saúl <1725110308@utectulancingo.edu.mx>
2026-07-13 02:12:38 +0000

    UPDATE se dejo funcionando el insert producto

--- controllers/productos/borrar_productos.py
+++ controllers/productos/borrar_productos.py
@@ -1,8 +1,8 @@
 import web
 import sqlite3
 
-render = web.template.render('views')
+render = web.template.render('views/productos')
 
 class BorrarProducto:
     def GET(self):
-        
+        return render.borrar_productos()
