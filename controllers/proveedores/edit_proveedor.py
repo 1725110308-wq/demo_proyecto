2b6bdf684a5663cc1c928702fe3ab007583ff1b8
Saúl <1725110308@utectulancingo.edu.mx>
2026-07-13 00:29:45 +0000

    UPDATE se dejo funcionando insertar

--- controllers/proveedores/edit_proveedor.py
+++ controllers/proveedores/edit_proveedor.py
@@ -2,6 +2,39 @@ import web
 import sqlite3
 render=web.template.render('views/proveedores',base='layout')
 class EditProveedor:
+    def editProveedor(self, proveedor: dict)-> bool:
+        try:
+            conexion=sqlite3.connect('sql/ferretariasaul.db')
+            conexion.row_factory=sqlite3.Row
+            cursor=conexion.cursor()
+            id_proveedor=proveedor['id_proveedor']
+            nombre=proveedor['nombre']
+            tipo=proveedor['tipo']
+            clase=proveedor['clase']
+            nombre_neg=proveedor['nombre_neg']
+            query="""UPDATE proveedores
+            SET nombre=?,
+                tipo=?,
+                clase=?,
+                nombre_neg=?
+            where id_proveedor=?;"""
+            datos=(
+                nombre,
+                tipo,
+                clase,
+                nombre_neg,
+                id_proveedor
+            )
+            cursor.execute(query,datos)
+            conexion.commit()
+            conexion.close()
+            return True
+        except sqlite3.Error as error:
+            print(f"Error 11: {error.args}")
+            return False
+        except Exception as errror:
+            print(f"Error 12: {errror.args}")
+            return False
     def mostrarProveedor(self,id_proveedor:int):
         try:
             conexion=sqlite3.connect('sql/ferretariasaul.db')
@@ -30,3 +63,16 @@ class EditProveedor:
         print(f"ID proveedor {id_proveedor}")
         proveedor=self.mostrarProveedor(id_proveedor)
         return render.edit_proveedor(proveedor)
+    def POST(self,id_proveedor:int):
+        formulario=web.input()
+        proveedor={
+            "id_proveedor":formulario['id_proveedor'],
+            "nombre":formulario['nombre'],
+            "tipo":formulario['tipo'],
+            "clase":formulario['clase'],
+            "nombre_neg":formulario['nombre_neg']
+        }
+        resultado=self.editProveedor(proveedor)
+        web.ctx.status = '303 See Other'
+        web.header('Location', '/ver_proveedores')
+        return ''
