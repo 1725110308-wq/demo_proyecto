abf723b843d379f45372b7b9dcd13149d0cebb5f
1725110526-maker <1725110526@utectulancingo.edu.mx>
2026-07-13 14:47:45 +0000

    UPDATE se dejo funcinoando editar

--- controllers/productos/editar_productos.py
+++ controllers/productos/editar_productos.py
@@ -2,6 +2,33 @@ import web
 import sqlite3
 render = web.template.render('views/productos', base='layout')
 class EditarProducto:
+    def editarProducto(self,producto:dict)->bool:
+        try:
+            conexion = sqlite3.connect('sql/ferreteriakory.db')
+            conexion.row_factory=sqlite3.Row
+            cursor = conexion.cursor()
+            id_productos = producto['id_productos']
+            precio = producto['precio']
+            cantidad = producto['cantidad']
+            calidad = producto['calidad']
+            descripcion = producto['descripcion']
+            query = """UPDATE productos
+                    SET precio=?,
+                        cantidad=?,
+                        calidad=?,
+                        descripcion=?
+                        WHERE id_productos=?"""
+            datos=(precio,cantidad,calidad,descripcion,id_productos)
+            cursor.execute(query,datos)
+            conexion.commit()
+            conexion.close()
+            return True
+        except sqlite3.Error as error:
+            print(f"Error 21: {error.args}")
+            return False
+        except Exception as errror:
+            print(f"Error 22: {errror.args}")
+            return False
     def mostrarProducto(self,id_productos):
         try:
             conexion = sqlite3.connect('sql/ferreteriakory.db')
@@ -32,3 +59,16 @@ class EditarProducto:
         print(f"ID del producto {id_productos}")
         prodcutos=self.mostrarProducto(id_productos)
         return render.edit_producto(prodcutos)
+    def POST(self, id_producto):
+        formulario=web.input()
+        producto={
+            "id_productos":formulario['id_productos'],
+            "precio":formulario['precio'],
+            "cantidad":formulario['cantidad'],
+            "calidad":formulario['calidad'],
+            "descripcion":formulario['descripcion']
+        }
+        self.editarProducto(producto)
+        web.ctx.status = '303 See Other'
+        web.header('Location', '/ver_productos')
+        return ''
